7c1b55f9c8dd7c46fe19d8455a9315718189c885
Dario Cancelliere <dario.cancelliere@gmail.com>
2022-08-14 11:07:27 +0200

    Dijkstra's algorithm

---
+++ tests/test_dijstra.rs
@@ -0,0 +1,42 @@
+use algorithms::dijkstra::{shortest_path, Edge};
+
+fn test_correct_minimum_cost() {
+    // This is the directed graph we're going to use.
+    // The node numbers correspond to the different states,
+    // and the edge weights symbolize the cost of moving
+    // from one node to another.
+    // Note that the edges are one-way.
+    //
+    //                  7
+    //          +-----------------+
+    //          |                 |
+    //          v   1        2    |  2
+    //          0 -----> 1 -----> 3 ---> 4
+    //          |        ^        ^      ^
+    //          |        | 1      |      |
+    //          |        |        | 3    | 1
+    //          +------> 2 -------+      |
+    //           10      |               |
+    //                   +---------------+
+    //
+    // The graph is represented as an adjacency list where each index,
+    // corresponding to a node value, has a list of outgoing edges.
+    //
+    // Chosen for its efficiency.
+    let graph = vec![
+        vec![Edge { node: 2, cost: 10 },
+             Edge { node: 1, cost: 1 }],
+        vec![Edge { node: 3, cost: 2 }],
+        vec![Edge { node: 1, cost: 1 },
+             Edge { node: 3, cost: 3 },
+             Edge { node: 4, cost: 1 }],
+        vec![Edge { node: 0, cost: 7 },
+             Edge { node: 4, cost: 2 }],
+        vec![]];
+
+    assert_eq!(shortest_path(&graph, 0, 1), Some(1));
+    assert_eq!(shortest_path(&graph, 0, 3), Some(3));
+    assert_eq!(shortest_path(&graph, 3, 0), Some(7));
+    assert_eq!(shortest_path(&graph, 0, 4), Some(5));
+    assert_eq!(shortest_path(&graph, 4, 0), None);
+}
